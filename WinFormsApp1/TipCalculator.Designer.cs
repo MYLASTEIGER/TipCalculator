f4315215d3234ea3866113bab1558540f932eb6f
myla steiger <mrs.steiger@gmail.com>
2022-01-28 09:05:08 -0500

    Add project files.

---
+++ WinFormsApp1/TipCalculator.Designer.cs
@@ -0,0 +1,143 @@
+﻿
+namespace WinFormsApp1 {
+    partial class TipCalculator {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing) {
+            if (disposing && (components != null)) {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent() {
+            this.l1 = new System.Windows.Forms.Label();
+            this.l2 = new System.Windows.Forms.Label();
+            this.l3 = new System.Windows.Forms.Label();
+            this.txtBillAmount = new System.Windows.Forms.TextBox();
+            this.txtTipPercent = new System.Windows.Forms.TextBox();
+            this.txtTotalTipAmount = new System.Windows.Forms.TextBox();
+            this.btnCalculate = new System.Windows.Forms.Button();
+            this.btnExit = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // l1
+            // 
+            this.l1.AutoSize = true;
+            this.l1.Font = new System.Drawing.Font("Reem Kufi", 11.25F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point);
+            this.l1.Location = new System.Drawing.Point(146, 52);
+            this.l1.Name = "l1";
+            this.l1.Size = new System.Drawing.Size(89, 28);
+            this.l1.TabIndex = 0;
+            this.l1.Text = "&Bill Amount:";
+            // 
+            // l2
+            // 
+            this.l2.AutoSize = true;
+            this.l2.Font = new System.Drawing.Font("Reem Kufi", 11.25F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point);
+            this.l2.Location = new System.Drawing.Point(146, 111);
+            this.l2.Name = "l2";
+            this.l2.Size = new System.Drawing.Size(41, 28);
+            this.l2.TabIndex = 1;
+            this.l2.Text = "20%";
+            // 
+            // l3
+            // 
+            this.l3.AutoSize = true;
+            this.l3.Font = new System.Drawing.Font("Reem Kufi", 11.25F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point);
+            this.l3.Location = new System.Drawing.Point(146, 172);
+            this.l3.Name = "l3";
+            this.l3.Size = new System.Drawing.Size(98, 28);
+            this.l3.TabIndex = 2;
+            this.l3.Text = "Total Tip Amt";
+            // 
+            // txtBillAmount
+            // 
+            this.txtBillAmount.Location = new System.Drawing.Point(268, 58);
+            this.txtBillAmount.Name = "txtBillAmount";
+            this.txtBillAmount.Size = new System.Drawing.Size(100, 23);
+            this.txtBillAmount.TabIndex = 3;
+            // 
+            // txtTipPercent
+            // 
+            this.txtTipPercent.Location = new System.Drawing.Point(268, 117);
+            this.txtTipPercent.Name = "txtTipPercent";
+            this.txtTipPercent.ReadOnly = true;
+            this.txtTipPercent.Size = new System.Drawing.Size(100, 23);
+            this.txtTipPercent.TabIndex = 4;
+            // 
+            // txtTotalTipAmount
+            // 
+            this.txtTotalTipAmount.Location = new System.Drawing.Point(268, 177);
+            this.txtTotalTipAmount.Name = "txtTotalTipAmount";
+            this.txtTotalTipAmount.ReadOnly = true;
+            this.txtTotalTipAmount.Size = new System.Drawing.Size(100, 23);
+            this.txtTotalTipAmount.TabIndex = 5;
+            // 
+            // btnCalculate
+            // 
+            this.btnCalculate.Font = new System.Drawing.Font("Segoe UI Semibold", 12F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point);
+            this.btnCalculate.Location = new System.Drawing.Point(146, 241);
+            this.btnCalculate.Name = "btnCalculate";
+            this.btnCalculate.Size = new System.Drawing.Size(222, 33);
+            this.btnCalculate.TabIndex = 6;
+            this.btnCalculate.Text = "&Calculate";
+            this.btnCalculate.UseVisualStyleBackColor = true;
+            this.btnCalculate.Click += new System.EventHandler(this.btnCalculate_Click);
+            // 
+            // btnExit
+            // 
+            this.btnExit.Location = new System.Drawing.Point(293, 298);
+            this.btnExit.Name = "btnExit";
+            this.btnExit.Size = new System.Drawing.Size(75, 23);
+            this.btnExit.TabIndex = 7;
+            this.btnExit.Text = "&Exit";
+            this.btnExit.UseVisualStyleBackColor = true;
+            this.btnExit.Click += new System.EventHandler(this.btnExit_Click);
+            // 
+            // TipCalculator
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(516, 404);
+            this.Controls.Add(this.btnExit);
+            this.Controls.Add(this.btnCalculate);
+            this.Controls.Add(this.txtTotalTipAmount);
+            this.Controls.Add(this.txtTipPercent);
+            this.Controls.Add(this.txtBillAmount);
+            this.Controls.Add(this.l3);
+            this.Controls.Add(this.l2);
+            this.Controls.Add(this.l1);
+            this.Name = "TipCalculator";
+            this.Text = "Tip Calculator";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label l1;
+        private System.Windows.Forms.Label l2;
+        private System.Windows.Forms.Label l3;
+        private System.Windows.Forms.TextBox txtBillAmount;
+        private System.Windows.Forms.TextBox txtTipPercent;
+        private System.Windows.Forms.TextBox txtTotalTipAmount;
+        private System.Windows.Forms.Button btnCalculate;
+        private System.Windows.Forms.Button btnExit;
+    }
+}
+
